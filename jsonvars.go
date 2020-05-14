f52f76b833828ff17559dfeeb16e197e4fc132a9
Abiola Ibrahim <abiola89@gmail.com>
2020-05-14 11:51:41 +0100

    minor code restructure

--- jsonvars.go
+++ jsonvars.go
@@ -1,10 +1,7 @@
 package jsonvars
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
-	"strings"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
@@ -15,19 +12,19 @@ import (
 
 // Interface guards
 var (
-	_ caddy.Provisioner           = (*JSONMiddleware)(nil)
-	_ caddyhttp.MiddlewareHandler = (*JSONMiddleware)(nil)
-	_ caddyfile.Unmarshaler       = (*JSONMiddleware)(nil)
+	_ caddy.Provisioner           = (*JSONVars)(nil)
+	_ caddyhttp.MiddlewareHandler = (*JSONVars)(nil)
+	_ caddyfile.Unmarshaler       = (*JSONVars)(nil)
 )
 
 func init() {
-	caddy.RegisterModule(JSONMiddleware{})
+	caddy.RegisterModule(JSONVars{})
 	httpcaddyfile.RegisterHandlerDirective("json_vars", parseCaddyfile)
 }
 
-// JSONMiddleware implements an HTTP handler that writes the
-// visitor's IP address to a file or stream.
-type JSONMiddleware struct {
+// JSONVars implements an HTTP handler that parses
+// json body as placeholders.
+type JSONVars struct {
 	Strict string `json:"strict,omitempty"`
 
 	strict bool
@@ -35,15 +32,15 @@ type JSONMiddleware struct {
 }
 
 // CaddyModule returns the Caddy module information.
-func (JSONMiddleware) CaddyModule() caddy.ModuleInfo {
+func (JSONVars) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
 		ID:  "http.handlers.json_vars",
-		New: func() caddy.Module { return new(JSONMiddleware) },
+		New: func() caddy.Module { return new(JSONVars) },
 	}
 }
 
 // Provision implements caddy.Provisioner.
-func (m *JSONMiddleware) Provision(ctx caddy.Context) error {
+func (m *JSONVars) Provision(ctx caddy.Context) error {
 	m.log = ctx.Logger(m)
 
 	if m.Strict == "strict" {
@@ -52,44 +49,11 @@ func (m *JSONMiddleware) Provision(ctx caddy.Context) error {
 	return nil
 }
 
-func (m JSONMiddleware) replacerFunc(r *http.Request) (caddy.ReplacerFunc, error) {
-	var v interface{}
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Body.Close()
-
-	err = json.Unmarshal(body, &v)
-	if err != nil {
-		return nil, err
-	}
-
-	// prevent repetitive parsing. cache values
-	keys := map[string]interface{}{}
-
-	return func(key string) (interface{}, bool) {
-		prefix := "json."
-		if !strings.HasPrefix(key, prefix) {
-			return nil, false
-		}
-		key = strings.TrimPrefix(key, prefix)
-
-		// use cache if previously fetched
-		if val, ok := keys[key]; ok {
-			return val, true
-		}
-
-		return fetchValue(v, key), true
-
-	}, nil
-}
-
 // ServeHTTP implements caddyhttp.MiddlewareHandler.
-func (m JSONMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+func (m JSONVars) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
 
-	replacerFunc, err := m.replacerFunc(r)
+	replacerFunc, err := newReplacerFunc(r)
 	if err != nil {
 		if m.strict {
 			return caddyhttp.Error(http.StatusBadRequest, err)
@@ -105,7 +69,7 @@ func (m JSONMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next c
 }
 
 // UnmarshalCaddyfile implements caddyfile.Unmarshaler.
-func (m *JSONMiddleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+func (m *JSONVars) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
 		if d.Args(&m.Strict) {
 			if m.Strict != "strict" {
@@ -118,7 +82,7 @@ func (m *JSONMiddleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 
 // parseCaddyfile unmarshals tokens from h into a new Middleware.
 func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
-	var m JSONMiddleware
+	var m JSONVars
 	err := m.UnmarshalCaddyfile(h.Dispenser)
 	return m, err
 }
--- parser.go
+++ parser.go
@@ -1,16 +1,30 @@
 package jsonvars
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/caddyserver/caddy/v2"
 )
 
-type fetcherFunc func(interface{}, string) (interface{}, bool)
-type fetcherFuncs []fetcherFunc
+type fetcher interface {
+	Fetch(interface{}, string) (interface{}, bool)
+}
 
-func (fs fetcherFuncs) fetch(v interface{}, key string) (interface{}, bool) {
+type fetcherFunc func(interface{}, string) (interface{}, bool)
+
+func (f fetcherFunc) Fetch(v interface{}, key string) (interface{}, bool) {
+	return f(v, key)
+}
+
+type fetchers []fetcher
+
+func (fs fetchers) Fetch(v interface{}, key string) (interface{}, bool) {
 	for _, f := range fs {
-		v, ok := f(v, key)
+		v, ok := f.Fetch(v, key)
 		if ok {
 			return v, true
 		}
@@ -54,14 +68,52 @@ func fromArray(v interface{}, key string) (interface{}, bool) {
 }
 
 func fetchValue(v interface{}, key string) interface{} {
-	funcs := fetcherFuncs{fromMap, fromArray}
+	f := fetchers{
+		fetcherFunc(fromMap),
+		fetcherFunc(fromArray),
+	}
+
 	var current interface{} = v
 	for _, k := range strings.Split(key, ".") {
-		val, ok := funcs.fetch(current, k)
+		val, ok := f.Fetch(current, k)
 		if !ok {
 			return nil
 		}
 		current = val
 	}
+
 	return current
+}
+
+func newReplacerFunc(r *http.Request) (caddy.ReplacerFunc, error) {
+	var v interface{}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	err = json.Unmarshal(body, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	// prevent repetitive parsing. cache values
+	keys := map[string]interface{}{}
+
+	return func(key string) (interface{}, bool) {
+		prefix := "json."
+		if !strings.HasPrefix(key, prefix) {
+			return nil, false
+		}
+		key = strings.TrimPrefix(key, prefix)
+
+		// use cache if previously fetched
+		if val, ok := keys[key]; ok {
+			return val, true
+		}
+
+		return fetchValue(v, key), true
+
+	}, nil
 }
